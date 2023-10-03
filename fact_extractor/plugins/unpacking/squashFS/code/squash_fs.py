91532d241470d3ae02058af3bdd85deb6f95337a
Marten Ringwelski <git@maringuu.de>
2023-10-03 21:45:30 +0200

    Don't add "fact_extracted" prefix
    To me it does not make sense why the prefix as added at all.
    Also how would you distinguish between a tar that contains only one
    directory "fact_extracted"?

--- fact_extractor/plugins/unpacking/squashFS/code/squash_fs.py
+++ fact_extractor/plugins/unpacking/squashFS/code/squash_fs.py
@@ -30,7 +30,20 @@ def unpack_function(file_path, tmp_dir):
     '''
     unpack_result = {}
     for unpacker, parameter in SQUASH_UNPACKER:
-        output = execute_shell_command(f'fakeroot {unpacker} {parameter} -d {tmp_dir}/fact_extracted {file_path}')
+        output = execute_shell_command(
+            " ".join(
+                [
+                    "fakeroot",
+                    str(unpacker),
+                    parameter,
+                    "-dest",
+                    str(tmp_dir),
+                    # We need to force here since "-dest" does not allow existing directories
+                    "-force",
+                    str(file_path),
+                ],
+            )
+        )
         if _unpack_success(tmp_dir):
             unpack_result['unpacking_tool'] = unpacker.name
             unpack_result['output'] = output
